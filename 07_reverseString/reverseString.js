e7f84975267be3a4b47318c357474aad7db69430
Nick Dexter <nickdex1@hotmail.com>
2025-05-06 20:06:54 +1000

    feat: complete 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let string = "";
 
+    for(let i = -1; i >= -word.length; i--)
+    {
+        string += word.at(i);
+    }
+
+    return string
 };
 
 // Do not edit below this line
