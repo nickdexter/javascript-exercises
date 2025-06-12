27833494921661096c232b5d79dffbf8718d321c
Nick Dexter <nickdex1@hotmail.com>
2025-06-12 22:43:45 +1000

    feat: complete 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	  let sum = 0;
+  numbers.forEach((num) => sum += Number(num));
+  
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let total = 1;
 
+  numbers.forEach((num) => total *= num);
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+
+  for(let i = num; i >=1 ; i--){
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
