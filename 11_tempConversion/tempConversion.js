1872e479ef5a0db833329d88a3612a2f4074b97e
Nick Dexter <nickdex1@hotmail.com>
2025-06-12 19:25:30 +1000

    feat: complete 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  c = (fahrenheit - 32) * 5/9;
+  return Math.round(c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  f = (celcius * 9 / 5) + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
