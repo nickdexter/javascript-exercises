88fef9b61efedc180a17c3768faa8187e9f3d49f
Nick Dexter <nickdex1@hotmail.com>
2025-06-12 23:25:02 +1000

    feat: complete 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let current = 0;
+    let prev1 = 1;
+    let prev2 = 0;
+    num = parseInt(num);
+    
+    if(num < 0){
+        return "OOPS";
+    }
+
+    if(num === 0 || num === 1){
+        return num;
+    }
+
+    for(let i = 2; i <= num; i++){
+        current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current
+    }
+
+    return current;
 
 };
 
